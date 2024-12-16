f3eda1d221df5f050520ab840a8899090c9e8e52
Harshit Anand <146337117+anandharshit712@users.noreply.github.com>
2024-12-16 13:44:34 +0530

    Time: 7 ms (92.13%), Space: 18.1 MB (27.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -8,8 +8,7 @@ class Solution:
                 left += 1
             while left < right and s_list[right] not in vowels:
                 right -= 1
-            if left < right:
-                s_list[left], s_list[right] = s_list[right], s_list[left]
-                left += 1
-                right -= 1
+            s_list[left], s_list[right] = s_list[right], s_list[left]
+            left += 1
+            right -= 1
         return ''.join(s_list)
